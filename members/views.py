216a040b38779e37870fbf81f2b119b95be947b5
Oluwaseun Samuel <samoluwaseun2016@gmail.com>
2026-06-22 20:47:37 +0100

    Added Djago Admin and more templates, view and url

--- members/views.py
+++ members/views.py
@@ -36,3 +36,34 @@ def details(request, id):
    'mymember': mymember, 
   } 
   return HttpResponse(template.render(context, request))
+
+def main(request): 
+    template = loader.get_template('main.html') 
+    return HttpResponse(template.render()) 
+
+
+# def testing(request): 
+#     template = loader.get_template('template.html') 
+#     context = { 
+#         'fruits': ['Apple', 'Banana', 'Cherry'], 
+#     } 
+#     return HttpResponse(template.render(context, request))
+
+
+# def testing(request): 
+#     template = loader.get_template('template.html') 
+#     context = { 
+#         'firstname': 'Linus',
+#     } 
+#     return HttpResponse(template.render(context, request))
+
+
+
+def testing(request): 
+    mymembers = Member.objects.all().values() 
+    template = loader.get_template('template.html') 
+    context = { 
+        'mymembers': mymembers, 
+        } 
+    return HttpResponse(template.render(context, request))
+
